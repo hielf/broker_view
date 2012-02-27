01493016a80dca64f2f3db5503794c0a87094e95
hielf <wercub@tom.com>
2012-02-27 17:42:26 +0800

    项目初始化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Htbroker::Application.config.secret_token = '4d0bbf7243de793c9008782264065caee1eae24801666be8a792022e8f36cd3b63aa6460c08a5a92be2f59d36f45970904ba3bdf72c8bdf1d8fcd8c9f6a83dcb'
