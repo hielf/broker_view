6318aed8c9b54a406cb38fdf842f8601df42c822
hielf <wercub@tom.com>
2012-03-07 16:16:27 +0800

    加入用户登陆验证

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,15 +27,36 @@ class User < ActiveRecord::Base
                        
   before_save :encrypt_password
   
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  class << self
+    def authenticate(email, submmited_password)
+      user = User.find_by_email(email)
+      return nil  if user.nil?
+      return user if user.has_password?(submmited_password)
+    end
+  end
+
+  
   #encrypted password method
   private 
     def encrypt_password
+      self.salt = make_salt if new_record?
       self.encrypted_password = encrypt(password)
     end
     
     def encrypt(string)
-      string 
+      secure_hash("#{salt}--#{string}") 
     end
     
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
 
