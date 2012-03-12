c8ee91c9905a4c20ed63cf1865f1ce0041710dfe
hielf <wercub@tom.com>
2012-03-13 01:54:40 +0800

    登录认证：错误用户

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,16 @@ class SessionsController < ApplicationController
   end
  
   def create
-    render 'new'
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "用户名/密码错误"
+      @title = "登录"
+      render 'new'
+    else
+      # handle successful signin
+      
+    end
   end
   
   def destroy
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,5 +15,31 @@ describe SessionsController do
       response.should have_selector('title', :content => "登录")
     end
   end
+  
+  describe "POST 'create'" do
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :email => "", :password => ""  }
+      end
+      
+      it "should re-render the new page" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+      
+      it "should have the right title " do
+        post :create, :session => @attr
+        response.should have_selector('title', :content => "登录")
+      end
+      
+      it "should have an error message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /错误/i
+      end
+    end
+    
+  end
 
 end
