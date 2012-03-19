138b753cc768d433890ad061784cac1f0caa6659
hielf <wercub@tom.com>
2012-03-20 00:44:17 +0800

    修改用户cookie持续时间为browser session close

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@
 module SessionsHelper
   
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    cookies.signed[:remember_token] = [user.id, user.salt]
     self.current_user = user
   end
   
