9ac348cbba80f00de4dc4e5d2e9a4b2474cf1478
hielf <wercub@tom.com>
2012-04-11 00:32:32 +0800

    working on branch view&dept view #3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-      redirect_back_or user #friendly redirect
+      redirect_back_or departments_path #user #friendly redirect
     end
   end
   
