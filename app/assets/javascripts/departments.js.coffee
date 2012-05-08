caf6b75d60bc61b4e935138885fc2ca384780adf
hielf <wercub@tom.com>
2012-05-09 03:20:37 +0800

    完成图表1

--- app/assets/javascripts/departments.js.coffee
+++ app/assets/javascripts/departments.js.coffee
@@ -2,3 +2,46 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 # alert gon.deptindex if gon
+# 
+# chart = undefined
+# $(document).ready ->
+#   Highcharts.setOptions lang:
+#     months: [ "一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月" ]
+# 
+#   new Highcharts.Chart(
+#     chart:
+#       renderTo: "chart_1"
+#       type: "column"
+# 
+#     title:
+#       text: "全公司收入"
+# 
+#     xAxis:
+#       type: "datetime"
+#       dateTimeLabelFormats:
+#         month: "%b '%y"
+# 
+#     yAxis:
+#       title:
+#         text: "单位：亿"
+# 
+#       min: 0
+#       labels:
+#         formatter: ->
+#           @value / 100000000
+# 
+#     credits:
+#       enabled: false
+# 	
+# 	series: [
+# 		 ( "总成交量" => Deptindex.matchbalance, "总净佣金" => Deptindex.commis ).each do |name, deptindices| 
+# 		# {
+# 		# 	name: "<%= name %>",
+# 		# 	pointInterval: <%= 1.month.to_i * 1000 %>,
+# 		# 	pointStart: <%= 1.year.ago.beginning_of_year.to_i * 1000 %>,
+# 		# 	data: <%= dept_chart_series(deptindices, Department.find_by_code(0000).id) %>
+# 		# 	# data: [['201101',5],['201102',7],['201103',4],['201104',5],['201105',8],['201106',3]]
+# 		# },
+# 		# <% end %>
+# 		]
+#   )
--- app/helpers/deptindices_helper.rb
+++ app/helpers/deptindices_helper.rb
@@ -1,15 +1,24 @@
 module DeptindicesHelper
   def dept_chart_series(deptindices, dept)
-    deptindex_by_month = deptindices.where(:month_id => 1.years.ago.to_date.at_beginning_of_year().strftime("%Y%m")..Time.now.beginning_of_month.strftime("%Y%m") ).select("month_id, occursum")
+    deptindex_by_month = deptindices.where(:month_id => 1.years.ago.to_date.strftime("%Y%m")..Time.now.beginning_of_month.strftime("%Y%m") ).select("month_id, occursum")
     now = Time.now
-    lastyear = Time.now.prev_year.beginning_of_year.prev_month
+    lastyear = Time.now.prev_year.prev_month
   # (1.years.ago.to_date.at_beginning_of_year().strftime("%Y%m")..Time.now.beginning_of_month.strftime("%Y%m")).map do |month|
     # ([ "201101", "201102", "201103" ]).map do |month|
-  (now.month + 12).times.map do
-    lastyear = lastyear.next_month
-    month = lastyear.strftime("%Y%m").to_s
+    (12).times.map do
+      lastyear = lastyear.next_month
+      month = lastyear.strftime("%Y%m").to_s
       sum = deptindex_by_month.detect { |sum| sum.month_id == month }
       sum && sum.occursum.to_f || 0
+    end
+  end
+  
+  def month_series
+    now = Time.now
+    lastyear = Time.now.prev_year.prev_month
+    (12).times.map do
+      lastyear = lastyear.next_month
+      month = lastyear.strftime("%Y%m").to_i
     end
   end
   
