2fa2ac57c3ab16d94a93eb4d6ca66b1a4d0275f3
hielf <wercub@tom.com>
2012-08-21 01:59:32 +0800

    finished broker_cust_view with highcharts

--- app/helpers/brokers_helper.rb
+++ app/helpers/brokers_helper.rb
@@ -3,13 +3,11 @@ module BrokersHelper
   
   def avatar
     @broker = Broker.find(params[:id])
-    image_tag(@broker.avatar, :alt => @broker.broker_name, :size => "90x90" )
+    if !@broker.avatar.nil?
+      image_tag(@broker.avatar, :alt => @broker.broker_name, :size => "90x90" )
+    else
+      image_tag("nobody.gif", :alt => @broker.broker_name, :size => "90x90" )
+    end 
   end
 
-  # def year_month
-  #   sd = Time.now.beginning_of_year.strftime("%m").to_i
-  #   ed = Time.now.strftime("%m").to_i
-  #   sd.upto(ed) { |month| puts month }
-  # end
-  
 end
